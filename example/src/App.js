ffaac09d5a2209026ae0e5c4a99549e7e89dc107
Guilherme Kammsetzer <guilherme@intruder.network>
2020-04-01 17:14:34 -0300

    feat: Add option to use hook

--- example/src/App.js
+++ example/src/App.js
@@ -1,26 +1,26 @@
 import React from "react";
 
-import { ReactCodeJar } from "react-codejar";
+import { useCodeJar } from "react-codejar";
 import "react-codejar/dist/index.css";
 
-const my = editor => {
+const highlight = editor => {
   let code = editor.textContent;
   code = code.replace(/\((\w+?)(\b)/g, '(<font color="#8a2be2">$1</font>$2');
   editor.innerHTML = code;
 };
 
 const App = () => {
-  const [code, setCode] = React.useState("(format t 'lol')");
+  const [code, onUpdate] = React.useState('(format t "lisp example")');
+
+  const editorRef = useCodeJar({
+    code,
+    onUpdate,
+    highlight
+  });
 
   return (
     <>
-      {code}
-      <ReactCodeJar
-        style={{ width: "200px", height: "200px", border: "1px solid black" }}
-        code={code}
-        onUpdate={setCode}
-        highlight={my}
-      />
+      <div ref={editorRef}></div>
     </>
   );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -9,27 +9,34 @@ interface Props {
   onUpdate: (code: string) => void;
 }
 
-export const ReactCodeJar: React.FC<Props> = props => {
+export const useCodeJar = (props: Props) => {
   const editorRef = React.useRef<HTMLDivElement>(null);
   let jar: CodeJar;
 
   React.useEffect(() => {
     if (!editorRef.current) return;
+
     jar = new CodeJar(editorRef.current, props.highlight, props.options);
     jar.onUpdate(props.onUpdate);
-
-    return jar.destroy;
+    return () => jar.destroy();
   }, [editorRef.current]);
 
   React.useEffect(() => {
     if (!jar) return;
+
     jar.updateCode(props.code);
   }, [props.code]);
 
   React.useEffect(() => {
     if (!jar || !props.options) return;
+
     jar.updateOptions(props.options);
   }, [props.options]);
 
+  return editorRef;
+};
+
+export const ReactCodeJar: React.FC<Props> = props => {
+  const editorRef = useCodeJar(props);
   return <div style={props.style} ref={editorRef}></div>;
 };
