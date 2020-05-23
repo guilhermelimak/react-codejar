533e6bdd6b062b86c4d74937dc03964287bd143d
Guilherme Kammsetzer <guilherme@intruder.network>
2020-05-23 19:16:14 -0300

    Updates

---
+++ precache-manifest.b6c4d28570c659bb3bf6768342d01669.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68e3643b4bd1f27f52d2bebc9aeda081",
+    "url": "/react-codejar/index.html"
+  },
+  {
+    "revision": "de7c8a4f26194aeed42e",
+    "url": "/react-codejar/static/css/2.6ea54503.chunk.css"
+  },
+  {
+    "revision": "f924e4a0cb516b66cafa",
+    "url": "/react-codejar/static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "de7c8a4f26194aeed42e",
+    "url": "/react-codejar/static/js/2.357c6198.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-codejar/static/js/2.357c6198.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f924e4a0cb516b66cafa",
+    "url": "/react-codejar/static/js/main.fb01e1bb.chunk.js"
+  },
+  {
+    "revision": "78848f294e8891c2e70e",
+    "url": "/react-codejar/static/js/runtime-main.d60afb14.js"
+  }
+]);
