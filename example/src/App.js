bdb4d296e751d3c82d50a3a11a32f81a425f4141
Guilherme Kammsetzer <guilherme@intruder.network>
2020-06-04 18:18:55 -0300

    feat(example): Add some changes
    Besides the styles missing it's probably not gonna stay this way, it's a bit complex without much benefit.
    After that is done I can link it on the README.

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,9 @@
 import React from "react";
-
-import { ReactCodeJar } from "react-codejar";
+import { useCodeJar, ReactCodeJar } from "react-codejar";
 import Prism from "prismjs";
 import "prismjs/themes/prism.css";
 
-const exampleCode = `const App = () => {
+const example = type => `const App = () => {
   const [code, onUpdate] = React.useState(exampleCode);
 
   const highlight = editor => {
@@ -17,6 +16,10 @@ const exampleCode = `const App = () => {
     );
   };
 
+  ${type}
+};`;
+
+const hooksCode = `
   const editorRef = useCodeJar({
     code,
     onUpdate,
@@ -31,8 +34,24 @@ const exampleCode = `const App = () => {
       <h5>Using component:</h5>
       <ReactCodeJar code={code} onUpdate={onUpdate} highlight={highlight} />
     </div>
-  );
-};`;
+  );`;
+
+const componentCode = `
+  const editorRef = useCodeJar({
+    code,
+    onUpdate,
+    highlight
+  });
+
+  return (
+    <div>
+      <h5>Using hooks: </h5>
+      <div ref={editorRef}></div>
+
+      <h5>Using component:</h5>
+      <ReactCodeJar code={code} onUpdate={onUpdate} highlight={highlight} />
+    </div>
+  );`;
 
 const highlight = editor => {
   const text = editor.textContent;
@@ -44,31 +63,44 @@ const highlight = editor => {
   );
 };
 
-const App = () => {
-  const [code, onUpdate] = React.useState(exampleCode);
+const Hook = () => {
+  const [code, onUpdate] = React.useState(example(hooksCode));
 
-  // const editorRef = useCodeJar({
-  //   code,
-  //   onUpdate,
-  //   highlight
-  // });
+  const editorRef = useCodeJar({
+    code,
+    onUpdate,
+    highlight
+  });
 
   return (
     <div>
-      {/* <h5>Using hooks: </h5> */}
-      {/* <div ref={editorRef}></div> */}
-
-      <h5>Using component:</h5>
-      <ReactCodeJar code={code} onUpdate={onUpdate} highlight={highlight} />
-      <button
-        onClick={() => {
-          onUpdate(code);
-        }}
-      >
-        Reset
-      </button>
+      <h5>Using hooks: </h5>
+      <div ref={editorRef}></div>
+      <button onClick={() => onUpdate(example(componentCode))}>Reset</button>
       <pre>{code}</pre>
     </div>
+  );
+};
+
+const Component = () => {
+  const [code, onUpdate] = React.useState(example(componentCode));
+
+  return (
+    <div>
+      <h5>Using component:</h5>
+      <ReactCodeJar code={code} onUpdate={onUpdate} highlight={highlight} />
+      <button onClick={() => onUpdate(example(componentCode))}>Reset</button>
+      <pre>{code}</pre>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <>
+      <Hook />
+      <Component />
+    </>
   );
 };
 
